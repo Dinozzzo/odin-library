4efe48a186834d0f580e4acead366b4ba98ee315
Dinozzzo <dleootta@gmail.com>
2025-09-05 02:15:04 +0200

    Just created the constructor and th e first book

---
+++ script.js
@@ -0,0 +1,14 @@
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", "300 pages", "Read");
+
+console.log(theHobbit);
+
+function addBooktoLibrary() {}
