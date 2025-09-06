70cf155cb93806e43374031801e40fdeab45a5ad
Dinozzzo <dleootta@gmail.com>
2025-09-06 16:52:51 +0200

    Need to create the form to add a book manually

--- script.js
+++ script.js
@@ -1,14 +1,60 @@
 const myLibrary = [];
 
-function Book(title, author, pages, read) {
+function Book(title, author, pages, read, id) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
+  this.id = id;
 }
 
-const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", "300 pages", "Read");
+function addBooktoLibrary(title, author, pages, read, id) {
+  const newBook = new Book(title, author, pages, read, id);
+  myLibrary.push(newBook);
+}
 
-console.log(theHobbit);
+addBooktoLibrary(
+  "The Hobbit",
+  "J.R.R. Tolkien",
+  "300 pages",
+  "Read",
+  crypto.randomUUID()
+);
 
-function addBooktoLibrary() {}
+addBooktoLibrary(
+  "Harry Potter",
+  "J.K Rowling",
+  "233 pages",
+  "Not read",
+  crypto.randomUUID()
+);
+
+// CREATION DU TABLEAU
+
+const tbody = document.querySelector("tbody");
+
+for (const book of myLibrary) {
+  // ROW
+  let row = document.createElement("tr");
+  tbody.appendChild(row);
+
+  // TITLE
+  let title = document.createElement("td");
+  title.textContent = book.title;
+  row.appendChild(title);
+
+  // AUTHOR
+  let author = document.createElement("td");
+  author.textContent = book.author;
+  row.appendChild(author);
+
+  // PAGES
+  let pages = document.createElement("td");
+  pages.textContent = book.pages;
+  row.appendChild(pages);
+
+  // STATUS
+  let status = document.createElement("td");
+  status.textContent = book.read;
+  row.appendChild(status);
+}
