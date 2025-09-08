676ffe024db51412198ca8c8b3d669d7d1fbe85a
Dinozzzo <dleootta@gmail.com>
2025-09-09 00:21:25 +0200

    Now the new row can be added via dialog

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ function Book(title, author, pages, read, id) {
 function addBooktoLibrary(title, author, pages, read, id) {
   const newBook = new Book(title, author, pages, read, id);
   myLibrary.push(newBook);
+  return newBook;
 }
 
 addBooktoLibrary(
@@ -33,42 +34,56 @@ addBooktoLibrary(
 
 const tbody = document.querySelector("tbody");
 
-for (const book of myLibrary) {
+function renderBookRow(book) {
   // ROW
   let row = document.createElement("tr");
   tbody.appendChild(row);
-
   // TITLE
   let title = document.createElement("td");
   title.textContent = book.title;
   row.appendChild(title);
-
   // AUTHOR
   let author = document.createElement("td");
   author.textContent = book.author;
   row.appendChild(author);
-
   // PAGES
   let pages = document.createElement("td");
   pages.textContent = book.pages;
   row.appendChild(pages);
-
   // STATUS
   let status = document.createElement("td");
   status.textContent = book.read;
   row.appendChild(status);
 }
 
+myLibrary.forEach(renderBookRow);
+
 // DIALOG
 
-const dialog = document.querySelector("dialog");
+const dialog = document.querySelector(".add-dialog");
 const showButton = document.querySelector(".createbook");
 const closeButton = document.querySelector(".closedialog");
+const form = document.querySelector("form");
 
 showButton.addEventListener("click", () => {
   dialog.showModal();
 });
 
-closeButton.addEventListener("click", () => {
-  dialog.close();
+dialog.addEventListener("close", () => {
+  if (dialog.returnValue === "valid") {
+    const fd = new FormData(form);
+    const title = fd.get("title");
+    const author = fd.get("author");
+    const pages = fd.get("pages");
+    const read = fd.get("read");
+    const book = addBooktoLibrary(
+      title,
+      author,
+      pages,
+      read,
+      crypto.randomUUID()
+    );
+    renderBookRow(book);
+    form.reset();
+  }
 });
